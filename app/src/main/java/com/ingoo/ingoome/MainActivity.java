4f4405d9e1fc7ad7179eab65890d8c41126c9aaf
Ingookey <Ingookey@gmail.com>
2017-03-05 23:15:36 +0800

    Add interface for InAdapter

--- app/src/main/java/com/ingoo/ingoome/MainActivity.java
+++ app/src/main/java/com/ingoo/ingoome/MainActivity.java
@@ -5,11 +5,15 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements InAdapter.InListener {
+    private static String TAG = "Ingoo/MainActivity";
+
+    private InAdapter mInAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +30,15 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mInAdapter = new InAdapter();
+        mInAdapter.setLinstener(this);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mInAdapter.moreBitmap();
     }
 
     @Override
@@ -49,4 +62,9 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+	
+	@Override
+	public void inListen() {
+		Log.i(TAG, "do something in activity");
+	}
 }
