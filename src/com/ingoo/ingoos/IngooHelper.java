39b450c0f12cac003d98e06bbe1feb9be35c99d6
Ingookey <Ingookey@gmail.com>
2017-03-08 22:20:59 +0800

    add helper and toast for ingoo

---
+++ src/com/ingoo/ingoos/IngooHelper.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.ingoo.ingoos;
+
+/**
+ *
+ */
+public class IngooHelper {
+	private static String TAG = "ingoo/IngooHelper";
+	
+	public final static int MSG_UPDATE_DATA = 1;
+	public final static int MSG_ERROR = 2;
+}
--- src/com/ingoo/ingoos/MainActivity.java
+++ src/com/ingoo/ingoos/MainActivity.java
@@ -5,30 +5,59 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.util.Log;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.ingoo.ingoos.utils.AppUtil;
 
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 	public static String TAG = "ingoo/MainActivity";
 	
-	public static int MSG_ITEM1 = 1; 
-	
 	private SyncHandler mSyncHandler = null;
+	private Button mButton1;
+	
+	public class SyncHandler extends Handler {
+		public SyncHandler() {  
+	    }  
+	 
+	    @Override
+	    public void handleMessage(Message msg) {
+			Log.d(TAG, "<handleMessage> msg.what: " + msg.what);
+	        super.handleMessage(msg);
+	        switch (msg.what) {
+			case IngooHelper.MSG_UPDATE_DATA:
+				updateData();
+				break;
+			case IngooHelper.MSG_ERROR:
+				break;
+			default:
+				break;
+			}
+	    }  
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		Log.w(TAG, "<onCreate> begin");
 		setContentView(R.layout.activity_main);
-		
-		mSyncHandler = new SyncHandler();
-		Message msg = new Message();
-		msg.what = MSG_ITEM1;
-		mSyncHandler.sendMessage(msg);
+		initializeData();
+		setupViews();
 		//AppUtil.buildSystemInfo(this);
+	}
+
+	private void initializeData() {
+		mSyncHandler = new SyncHandler();
+	}
+	
+	private void setupViews() {
+		mButton1 = (Button) findViewById(R.id.item_test2);
 	}
 
 	@Override
@@ -45,21 +74,27 @@ public class MainActivity extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	public class SyncHandler extends Handler {
-		public SyncHandler() {  
-	    }  
-	 
-	    @Override  
-	    public void handleMessage(Message msg) {  
-	        super.handleMessage(msg);
-	        switch (msg.what) {
-			case MSG_ITEM1:
-				
-				break;
-			default:
-				break;
-			}
-	    }  
+
+	public void updateData() {
+		Log.d(TAG, "<updateData>");
+		
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		//mSyncHandler.sendEmptyMessage(IngooHelper.MSG_ERROR);
+	}
+
+	@Override
+	public void onClick(View view) {
+		Log.d(TAG, "<onClick> view: " + view);
+		if (view == mButton1) {
+			Toast toast = Toast.makeText(this, "…Ë÷√≥…π¶", Toast.LENGTH_SHORT);
+			toast.setGravity(Gravity.CENTER, 0, 0);
+			toast.show();
+		} else {
+
+		}
 	}
 }
